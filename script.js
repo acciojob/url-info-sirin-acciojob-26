c086920d555f3e4bbe7dba8b623c68b377a9b95d
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-07-18 12:59:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+onst currentURL = window.location.href;
+
+// Get the length of the URL
+const urlLength = currentURL.length;
+
+// Print the length of the URL using alert
+alert(`The length of the URL is: ${urlLength}`);
+
